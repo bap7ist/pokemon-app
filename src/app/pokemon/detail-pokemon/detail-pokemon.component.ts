6288684744523b9f7a9bcb2e2d69ff5f724f39d0
Baptiste Roux <baptiste.c.roux@gmail.com>
2022-05-16 16:42:12 +0200

    firts commit

---
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-detail-pokemon',
+  templateUrl: './detail-pokemon.component.html',
+  styles: [
+  ]
+})
+export class DetailPokemonComponent implements OnInit {
+
+  pokemonList : Pokemon[];
+  pokemon: Pokemon|undefined;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private pokemonService: PokemonService
+    ) { }
+
+  ngOnInit() {
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+    if (pokemonId) { // Si pokemonId n'est pas null
+      this.pokemon = this.pokemonService.getPokemonById(+pokemonId);
+    }
+  }
+
+  goToPokemonList() {
+    this.router.navigate(['/pokemons']);
+  }
+
+  goToEditPokemon(pokemon: Pokemon) {
+    this.router.navigate(['/edit/pokemon', pokemon.id]);
+  }
+
+}
